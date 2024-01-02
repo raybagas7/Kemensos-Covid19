f042a601c9d39a5a9d520b4840d42d59c088a1fc
raybagas7 <raysam.gas@gmail.com>
2024-01-02 16:45:16 +0700

    feat(home): - add navigation link to go back home by clicking jds logo

--- src/components/Navigation/TopNavigation.jsx
+++ src/components/Navigation/TopNavigation.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const TopNavigation = ({ toggleSideNav, navHeight, setNavHeight }) => {
   const topNavbarRef = useRef(null);
@@ -28,13 +29,17 @@ const TopNavigation = ({ toggleSideNav, navHeight, setNavHeight }) => {
   return (
     <nav className="2 fixed top-0 z-[100] w-full border-[1px] bg-background/60 px-3 py-3 backdrop-blur md:px-28">
       <div>
-        <Image
-          alt="jds-logo"
-          src="/logo/logo_jds.png"
-          className="h-8 w-auto"
-          width={200}
-          height={100}
-        />
+        <Link href={"/"}>
+          <div className="w-fit cursor-pointer rounded-full border-[1px] border-border bg-white p-1 px-3 hover:bg-primary/20">
+            <Image
+              alt="jds-logo"
+              src="/logo/logo_jds.png"
+              className="h-7 w-auto"
+              width={200}
+              height={100}
+            />
+          </div>
+        </Link>
       </div>
     </nav>
   );
