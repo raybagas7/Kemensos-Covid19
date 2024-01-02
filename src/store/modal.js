0555845d4fea9397119f8c3f62451c3d1502ab6c
raybagas7 <raysam.gas@gmail.com>
2024-01-02 14:49:05 +0700

    fix(zustand store): - fix modal store: remove unused state and action

--- src/store/modal.js
+++ src/store/modal.js
@@ -3,11 +3,6 @@ import { create } from "zustand";
 export const useModal = create((set) => ({
   show: false,
   content: null,
-  showUpSlide: false,
-  contentUpslide: null,
   showModal: (content) => set(() => ({ show: true, content })),
   hideModal: () => set(() => ({ show: false })),
-  showModalUpSlide: (contentUpslide) =>
-    set(() => ({ showUpSlide: true, contentUpslide })),
-  hideModalUpSlide: () => set(() => ({ showUpSlide: false })),
 }));
