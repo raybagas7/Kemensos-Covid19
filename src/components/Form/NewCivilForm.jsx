97687aa12c3053b63aa519cdfe66ebd0a8d35b60
raybagas7 <raysam.gas@gmail.com>
2024-01-03 11:59:08 +0700

    feat(deps): - add shadcn form to the deps

---
+++ src/components/Form/NewCivilForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const NewCivilForm = () => {
+  return <div>NewCivilForm</div>;
+};
+
+export default NewCivilForm;
