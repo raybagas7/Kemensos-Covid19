005922d691ebb5244f7718a8272a295355e4378e
raybagas7 <raysam.gas@gmail.com>
2024-01-05 13:05:36 +0700

    feat(detail civil page): - optimize mobile view - add modal to show bigger image when the document image clicked

--- src/pages/civil/[nik].jsx
+++ src/pages/civil/[nik].jsx
@@ -3,11 +3,28 @@ import CoreLayout from "@/components/Layout/CoreLayout";
 import Modal from "@/components/Modal/Modal";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { formatIDR } from "@/lib/utils";
+import { useModal } from "@/store/modal";
 import Image from "next/image";
 
 import React from "react";
 
 const Civil = ({ dataSipil }) => {
+  const { showModal } = useModal();
+
+  const showBiggerImage = (url) => {
+    showModal(
+      <div className="relative rounded-lg bg-background p-1">
+        <Image
+          className="max-h-[500px] rounded-lg border object-contain"
+          src={url}
+          alt="selected-project-picture"
+          width={1020}
+          height={720}
+        />
+      </div>,
+    );
+  };
+
   let rgx = new RegExp(/(\p{L}{1})\p{L}+/, "gu");
 
   let initials = [...dataSipil.nama.matchAll(rgx)] || [];
@@ -21,15 +38,15 @@ const Civil = ({ dataSipil }) => {
       <Modal backDropClose />
       <h1 className="text-center text-2xl font-bold">Data Sipil</h1>
       <div className="mt-5">
-        <div className="flex gap-3">
-          <Avatar className="h-64 w-64 rounded-lg border-[1px] border-border">
+        <div className="flex gap-3 max-md:flex-col max-md:items-center">
+          <Avatar className="h-64 w-64 rounded-lg border-[1px] border-border max-md:h-40 max-md:w-40">
             <AvatarImage
               className=" object-cover"
               src={`https://dummyimage.com/360x360/FFCC29/ffffff.jpg&text=${initials}`}
             />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
-          <div className="flex ">
+          <div className="flex max-md:justify-center max-md:text-sm">
             <div className="flex gap-3">
               <div className="flex flex-col justify-between font-bold">
                 <p>Nama</p>
@@ -56,13 +73,13 @@ const Civil = ({ dataSipil }) => {
             </div>
           </div>
         </div>
-        <div className="mt-5 flex w-full gap-5">
+        <div className="mb-5 mt-5 flex w-full gap-5 max-md:flex-col max-md:text-sm">
           <div className="flex-1 space-y-5">
             <LabeledContainer label="Alamat Lengkap">
               <p>{dataSipil.alamat}</p>
             </LabeledContainer>
             <LabeledContainer className="flex" label="Pendapatan">
-              <div className="flex-1">
+              <div className="flex-1 ">
                 <p className="font-bold">Gaji Sebelum Pandemi</p>
                 <p
                   className={`${
@@ -95,10 +112,10 @@ const Civil = ({ dataSipil }) => {
           </div>
           <div className="flex-[1.5]">
             <LabeledContainer label="Data Dokumen">
-              <div className="mb-5 flex gap-3">
+              <div className="mb-5 flex gap-3 ">
                 <div className="flex flex-col justify-between font-bold">
-                  <p>Nomor Induk Kependudukan</p>
-                  <p>Nomor Kartu Keluarga</p>
+                  <p className=" line-clamp-1">Nomor Induk Kependudukan</p>
+                  <p className=" line-clamp-1">Nomor Kartu Keluarga</p>
                 </div>
                 <div className="flex flex-col justify-between">
                   <p>{dataSipil.nik}</p>
@@ -106,20 +123,22 @@ const Civil = ({ dataSipil }) => {
                 </div>
               </div>
               <LabeledContainer
-                className="flex w-fit gap-3"
+                className="flex w-fit gap-3 max-md:w-full max-md:flex-col"
                 label={"Foto Dokumen"}
               >
                 <Image
+                  onClick={() => showBiggerImage(dataSipil.foto_ktp)}
                   alt="ktp-sipil"
                   src={dataSipil.foto_ktp}
-                  className="h-auto min-w-56 cursor-pointer rounded-lg border-[1px] border-border hover:border-primary"
+                  className="h-auto min-w-40 cursor-pointer rounded-lg border-[1px] border-border hover:border-primary max-md:self-center"
                   width={500}
                   height={500}
                 />
                 <Image
+                  onClick={() => showBiggerImage(dataSipil.foto_kk)}
                   alt="ktp-sipil"
                   src={dataSipil.foto_kk}
-                  className="h-auto min-w-56 cursor-pointer rounded-lg border-[1px] border-border hover:border-primary"
+                  className="h-auto min-w-40 cursor-pointer rounded-lg border-[1px] border-border hover:border-primary max-md:self-center"
                   width={500}
                   height={500}
                 />
